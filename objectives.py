25c00dd4c2686e7189b4234b1f8f8d40555dc073
Vlad Shmyhlo <aion.stu@gmail.com>
2018-02-16 09:48:31 +0200

    Small fix

--- objectives.py
+++ objectives.py
@@ -37,15 +37,15 @@ def level_loss(labels, logits, name='level_loss'):
 
     class_loss = focal_sigmoid_cross_entropy_with_logits(
         labels=labels[0], logits=logits[0])
-    class_loss = tf.reduce_mean(class_loss)
+    # class_loss = tf.reduce_mean(class_loss)
 
     regr_loss = tf.square(labels[1] - logits[1])
-    regr_loss = tf.cond(
-        tf.reduce_sum(tf.to_float(non_background_mask)) > 0,
-        lambda: tf.reduce_mean(tf.boolean_mask(regr_loss, non_background_mask)),
-        lambda: 0.0)
-    # regr_loss = regr_loss * non_background_mask
-    # regr_loss = (regr_loss)
+    regr_loss = tf.boolean_mask(regr_loss, non_background_mask)
+
+    # regr_loss = tf.cond(
+    #     tf.reduce_sum(tf.to_float(non_background_mask)) > 0,
+    #     lambda: tf.reduce_mean(tf.boolean_mask(regr_loss, non_background_mask)),
+    #     lambda: 0.0)
 
   return class_loss, regr_loss
 
@@ -60,11 +60,12 @@ def loss(true, pred, name='loss'):
     with tf.control_dependencies(validate_output_shapes(true, pred)):
       for ct, rt, cp, rp in zip(*true, *pred):
         class_loss, regr_loss = level_loss((ct, rt), (cp, rp))
-        # regr_loss = tf.Print(regr_loss, [regr_loss])
         class_losses.append(class_loss)
         regr_losses.append(regr_loss)
 
-    class_loss = sum(class_losses)  # sum over python list of values
-    regr_loss = sum(regr_losses)  # sum over python list of values
+    class_loss = sum(tf.reduce_sum(l) for l in class_losses) / sum(
+        tf.size(l) for l in class_losses)  # sum over python list of values
+    regr_loss = sum(tf.reduce_sum(l) for l in regr_losses) / sum(
+        tf.size(l) for l in regr_losses)  # sum over python list of values
 
   return class_loss, regr_loss
