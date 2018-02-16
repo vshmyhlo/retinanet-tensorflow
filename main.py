80862d2f45293d672b4c3efc8be89654117c0811
Vlad Shmyhlo <aion.stu@gmail.com>
2018-02-16 11:05:49 +0200

    Small fix

--- main.py
+++ main.py
@@ -127,8 +127,9 @@ def main():
     ])
     image_summary = tf.summary.merge([
         tf.summary.image('boxmap', tf.expand_dims(image_with_boxes, 0)),
-        # tf.summary.histogram('predicted_classes',
-        #                      tf.argmax(classifications_pred, -1))
+        tf.summary.histogram('predicted_classes',
+                             tf.reshape(
+                                 tf.argmax(classifications_pred, -1), (-1, )))
     ])
 
   locals_init = tf.local_variables_initializer()
