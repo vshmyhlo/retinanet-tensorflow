96d8316c7cb93ffd9a575e77854ed0235429f036
Vlad Shmyhlo <aion.stu@gmail.com>
2018-02-16 23:57:02 +0200

    Refactor

--- download_weights.sh
+++ download_weights.sh
@@ -1,2 +1,4 @@
-wget -P pretrained http://download.tensorflow.org/models/resnet_v1_50_2016_08_28.tar.gz
 wget -P pretrained http://download.tensorflow.org/models/resnet_v2_50_2017_04_14.tar.gz
+mkdir pretrained/resnet_v2_50
+tar zxvf pretrained/resnet_v2_50_2017_04_14.tar.gz -C pretrained/resnet_v2_50
+rm pretrained/resnet_v2_50_2017_04_14.tar.gz
