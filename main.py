16c5b6064df2ab24c0e174c61473f410f9f86793
Vlad Shmyhlo <aion.stu@gmail.com>
2018-02-16 16:38:09 +0200

    Refactor

--- main.py
+++ main.py
@@ -21,11 +21,11 @@ def draw_heatmap(image, classification):
   heatmap = tf.reduce_max(heatmap, -1)
   heatmap = tf.not_equal(heatmap, 0)
   heatmap = tf.to_float(heatmap)
-  # heatmap = tf.expand_dims(heatmap, -1)
+  heatmap = tf.expand_dims(heatmap, -1)
   heatmap = tf.image.resize_images(
       heatmap, image_size, method=tf.image.ResizeMethod.AREA)
 
-  image_with_heatmap = image * tf.stack([
+  image_with_heatmap = image * tf.concat([
       heatmap * 0.5 + (1 - heatmap),
       tf.ones_like(heatmap),
       heatmap * 0.5 + (1 - heatmap),
