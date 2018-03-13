af53ef56f9fd70140f0d4de1e5b11ec08e30ad89
Vlad Shmyhlo <aion.stu@gmail.com>
2018-03-13 17:02:35 +0200

    Refactor

---
+++ cv_utils_test.py
@@ -0,0 +1,34 @@
+import numpy as np
+import cv_utils
+
+
+def test_iou():
+    iou = cv_utils.iou(
+        np.array([
+            [0.1, 0.1, 0.2, 0.2],
+            [100, 100, 200, 200],
+            [0.1, 0.1, 0.2, 0.2],
+        ]),
+        np.array([
+            [0.1, 0.1, 0.3, 0.3],
+            [100, 100, 300, 300],
+            [100, 100, 300, 300],
+        ]))
+
+    assert np.allclose(iou, [0.25, 0.25, 0])
+
+
+def test_relative_iou():
+    iou = cv_utils.relative_iou(
+        np.array([
+            [0.15, 0.15, 0.1, 0.1],
+            [150, 150, 100, 100],
+            [0.15, 0.15, 0.1, 0.1],
+        ]),
+        np.array([
+            [0.2, 0.2, 0.2, 0.2],
+            [200, 200, 200, 200],
+            [200, 200, 200, 200],
+        ]))
+
+    assert np.allclose(iou, [0.25, 0.25, 0])
