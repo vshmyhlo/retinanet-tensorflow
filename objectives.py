7e52cbaabaa7ae39a65dbc16f51b20ae31204423
Vlad Shmyhlo <aion.stu@gmail.com>
2018-03-13 15:48:36 +0200

    Refactor

--- objectives.py
+++ objectives.py
@@ -31,19 +31,19 @@ def loss(labels, logits, name='loss'):
 
         non_background_mask = tf.not_equal(tf.argmax(labels[0], -1), 0)
 
-        with tf.control_dependencies([
-                tf.assert_none_equal(
-                    tf.reduce_sum(tf.to_float(non_background_mask)), 0)
-        ]):
+        # with tf.control_dependencies([
+        #         tf.assert_none_equal(
+        #             tf.reduce_sum(tf.to_float(non_background_mask)), 0)
+        # ]):
 
-            class_loss = focal_sigmoid_cross_entropy_with_logits(
-                labels=labels[0], logits=logits[0])
-            class_loss = tf.reduce_sum(class_loss) / tf.reduce_sum(
-                tf.to_float(non_background_mask))
+        class_loss = focal_sigmoid_cross_entropy_with_logits(
+            labels=labels[0], logits=logits[0])
+        class_loss = tf.reduce_sum(class_loss) / tf.reduce_sum(
+            tf.to_float(non_background_mask))
 
-            regr_loss = tf.losses.huber_loss(
-                labels[1], logits[1], reduction=tf.losses.Reduction.NONE)
-            regr_loss = tf.reduce_sum(
-                tf.boolean_mask(regr_loss, non_background_mask))
+        regr_loss = tf.losses.huber_loss(
+            labels[1], logits[1], reduction=tf.losses.Reduction.NONE)
+        regr_loss = tf.reduce_sum(
+            tf.boolean_mask(regr_loss, non_background_mask))
 
     return class_loss, regr_loss
