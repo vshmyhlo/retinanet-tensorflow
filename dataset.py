3ed3c52356910bea5e02e0f22d662d932d0689e3
Vlad Shmyhlo <aion.stu@gmail.com>
2018-03-18 20:43:07 +0200

    Refactor

--- dataset.py
+++ dataset.py
@@ -15,49 +15,6 @@ def level_labels(image_size, class_ids, boxes, level):
     grid_size = tf.to_int32(tf.ceil(image_size / 2**level.number))
     anchor_boxes = tf.to_float(level.anchor_boxes / image_size)
 
-    # build grid anchor positions ##############################################
-    # cell_size = image.size / grid_size
-    # grid_y_positions = np.linspace(
-    #     cell_size[0] / 2, image.size[0] - cell_size[0] / 2,
-    #     grid_size[0]).reshape((grid_size[0], 1))  # [H, 1]
-    # grid_x_positions = np.linspace(
-    #     cell_size[1] / 2, image.size[1] - cell_size[1] / 2,
-    #     grid_size[1]).reshape((1, grid_size[1]))  # [1, W]
-    # del cell_size
-
-    # grid_y_positions = np.tile(grid_y_positions, (1, grid_size[1]))  # [H, W]
-    # grid_x_positions = np.tile(grid_x_positions, (grid_size[0], 1))  # [H, W]
-    # assert grid_x_positions.shape == grid_y_positions.shape
-    #
-    # grid_anchor_positions = np.stack([grid_y_positions, grid_x_positions],
-    #                                  -1)  # [H, W, 2]
-    # del grid_x_positions, grid_y_positions
-    #
-    # # build grid anchor sizes ##################################################
-    # grid_anchor_sizes = np.array(
-    #     [
-    #         compute_box_size(level.anchor_size, aspect_ratio, scale_ratio)
-    #         for aspect_ratio, scale_ratio in level.anchor_aspect_scale_ratios
-    #     ],
-    #     dtype=np.float32)  # [SIZES, 2]
-    #
-    # # build grid anchors #######################################################
-    # num_ratios = len(level.anchor_aspect_scale_ratios)
-    # grid_anchor_positions = grid_anchor_positions.reshape((*grid_size, 1,
-    #                                                        2))  # [H, W, 1, 2]
-    # grid_anchor_positions = np.tile(grid_anchor_positions,
-    #                                 (1, 1, num_ratios, 1))  # [H, W, SIZES, 2]
-    #
-    # grid_anchor_sizes = grid_anchor_sizes.reshape((1, 1, num_ratios,
-    #                                                2))  # [1, 1, SIZES, 2]
-    # grid_anchor_sizes = np.tile(grid_anchor_sizes,
-    #                             (*grid_size, 1, 1))  # [H, W, SIZES, 2]
-    #
-    # anchor_box_grid = np.concatenate(
-    #     [grid_anchor_positions, grid_anchor_sizes], -1)  # [H, W, SIZES, 4]
-    # del grid_anchor_positions, grid_anchor_sizes
-    #
-
     # extract targets ##########################################################
 
     # [OBJECTS]
@@ -67,15 +24,11 @@ def level_labels(image_size, class_ids, boxes, level):
     boxes_true = tf.to_float(
         boxes_true / tf.concat([image_size, image_size], 0))
 
-    # assert classes_true.shape[0] == boxes_true.shape[0]
-
     # compute iou ##############################################################
 
     # [OBJECTS, 1, 1, 1, 4]
     boxes_true_shape = tf.shape(boxes_true)
     boxes_true = tf.reshape(boxes_true, (boxes_true_shape[0], 1, 1, 1, 4))
-
-    # anchor_box_grid = np.expand_dims(anchor_box_grid, 0)  # [1, H, W, SIZES, 4]
 
     # [1, H, W, SIZES, 4]
     anchor_boxmap = utils.anchor_boxmap(grid_size, anchor_boxes)
@@ -88,23 +41,12 @@ def level_labels(image_size, class_ids, boxes, level):
     indices = tf.argmax(iou, 0)
     del iou
 
-    # assert indices.shape == (*grid_size, num_ratios)
-
     # build classification targets #############################################
 
     # [H, W, SIZES]
     classification = tf.gather(classes_true, indices)
 
-    # assert classification.shape == (*grid_size, num_ratios)
-
     # build regression targets #################################################
-    # shifts = (boxes_true[..., :2] - anchor_box_grid[..., :2]
-    #           ) / anchor_box_grid[..., 2:]  # [OBJECTS, H, W, SIZES, 2]
-    # scales = boxes_true[..., 2:] / anchor_box_grid[
-    #     ..., 2:]  # [OBJECTS, H, W, SIZES, 2]
-    # shift_scales = np.concatenate([shifts, scales],
-    #                               -1)  # [OBJECTS, H, W, SIZES, 4]
-    # del shifts, scales
 
     # [H, W, SIZES, 1]
     indices_expanded = tf.expand_dims(indices, -1)
@@ -117,17 +59,6 @@ def level_labels(image_size, class_ids, boxes, level):
     regression = boxes_true * indices_expanded
     # [H, W, SIZES, 4]
     regression = tf.reduce_sum(regression, 0)
-
-    # [H, W, SIZES, 4]
-    # anchor_boxmap_shape = tf.shape(anchor_boxmap)
-    # regression = tf.zeros(anchor_boxmap_shape[1:], dtype=np.float32)
-
-    # print(boxes_true)
-    # print(indices_expanded)
-    # fail
-    # for i in range(classes_true.shape[0]):
-    #     mask = tf.equal(indices_expanded, i)
-    #     regression += boxes_true[i] * tf.to_float(mask)
 
     return classification, regression
 
@@ -150,6 +81,9 @@ def gen(coco):
         anns = coco.load_anns(coco.get_ann_ids(img_ids=img.id))
         class_ids = np.array([item.category_id for item in anns])
         boxes = np.array([item.box for item in anns])
+
+        assert class_ids.shape[0] != 0
+        assert boxes.shape[0] != 0
 
         yield filename, class_ids, boxes
 
