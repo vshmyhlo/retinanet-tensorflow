3494a022ef9e58201d0197dbb629c3ea987dd23c
Vlad Shmyhlo <aion.stu@gmail.com>
2018-04-22 17:54:40 +0300

    Refactor

--- objectives.py
+++ objectives.py
@@ -89,6 +89,6 @@ def loss(labels, logits, not_ignored_mask, name='loss'):
         regr_loss = regression_loss(
             labels=regr_labels,
             logits=regr_logits,
-            non_background_mask=non_background_mask) * 0.  # TODO:
+            non_background_mask=non_background_mask)
 
         return class_loss, regr_loss
