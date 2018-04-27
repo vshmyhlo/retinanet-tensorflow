9ea5e93005616c552222d1c5a683fb52e0285f76
Vlad Shmyhlo <aion.stu@gmail.com>
2018-04-27 13:05:20 +0300

    Refactor

--- train.py
+++ train.py
@@ -62,7 +62,8 @@ def draw_bounding_boxes(image, regressions, classifications, max_output_size=100
     # TODO: add nms
     # final_scores = tf.concat(final_scores, 0)
     # nms_indices = tf.image.non_max_suppression(final_boxes, final_scores, max_output_size, iou_threshold=0.5)
-    final_boxes = tf.expand_dims(tf.gather(final_boxes, nms_indices), 0)
+    # final_boxes = tf.gather(final_boxes, nms_indices)
+    final_boxes = tf.expand_dims(final_boxes, 0)
 
     image = tf.image.draw_bounding_boxes(image, final_boxes)
     image = tf.squeeze(image, 0)
