874df5c7470afbf80e5cd5363511d4c86ee1a52b
Vlad Shmyhlo <aion.stu@gmail.com>
2018-06-27 20:44:50 +0300

    Refactor

--- losses.py
+++ losses.py
@@ -46,11 +46,11 @@ def focal_softmax_cross_entropy_with_logits(
 def classification_loss(labels, logits, non_bg_mask):
     # TODO: check bg mask usage and bg weighting calculation
 
-    bce = balanced_sigmoid_cross_entropy_with_logits(labels=labels, logits=logits)
+    # bce = balanced_sigmoid_cross_entropy_with_logits(labels=labels, logits=logits)
     dice = dice_loss(labels=labels, logits=logits, axis=0)
 
     loss = sum([
-        tf.reduce_mean(bce),
+        # tf.reduce_mean(bce),
         tf.reduce_mean(dice),
     ])
 
