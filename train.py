0e213447410d91d482f3cc44e92e2984502d86e3
Vlad Shmyhlo <aion.stu@gmail.com>
2018-07-03 22:17:25 +0300

    Refactor

--- train.py
+++ train.py
@@ -213,7 +213,8 @@ def build_summary(metrics, image, labels, logits, learning_rate, class_names):
 
 
 def build_learning_rate(global_step, config):
-    return cyclical_learning_rate(1e-3, 3., 5000, global_step)
+    # return cyclical_learning_rate(1e-3, 3., 5000, global_step)
+    return config.learning_rate
 
 
 def main():
