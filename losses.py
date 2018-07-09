5bad567ff21abe611344eb51bf6c28c7f594a9e5
Vlad Shmyhlo <aion.stu@gmail.com>
2018-07-09 16:45:33 +0300

    Refactor

--- losses.py
+++ losses.py
@@ -35,15 +35,15 @@ def focal_softmax_cross_entropy_with_logits(
 
 
 # TODO: check bg mask usage and bg weighting calculation
-def classification_loss(labels, logits, non_bg_mask, class_loss_kwargs):
+def classification_loss(labels, logits, fg_mask, class_loss_kwargs):
     losses = []
 
     # focal = focal_sigmoid_cross_entropy_with_logits(labels=labels, logits=logits, **class_loss_kwargs)
-    # num_non_bg = tf.reduce_sum(tf.to_float(non_bg_mask))
-    # focal = tf.reduce_sum(focal) / tf.maximum(num_non_bg, 1.0)  # TODO: count all points, not only trainable?
+    # num_fg = tf.reduce_sum(tf.to_float(fg_mask))
+    # focal = tf.reduce_sum(focal) / tf.maximum(num_fg, 1.0)  # TODO: count all points, not only trainable?
     # losses.append(focal)
 
-    bce = balanced_sigmoid_cross_entropy_with_logits(labels=labels, logits=logits, non_bg_mask=non_bg_mask)
+    bce = balanced_sigmoid_cross_entropy_with_logits(labels=labels, logits=logits, fg_mask=fg_mask)
     losses.append(bce)
 
     dice = dice_loss(labels=labels, logits=logits, axis=0)
@@ -57,11 +57,11 @@ def classification_loss(labels, logits, non_bg_mask, class_loss_kwargs):
     return loss
 
 
-def regression_loss(labels, logits, non_bg_mask):
+def regression_loss(labels, logits, fg_mask):
     loss = tf.losses.huber_loss(
         labels=labels,
         predictions=logits,
-        weights=tf.expand_dims(non_bg_mask, -1),
+        weights=tf.expand_dims(fg_mask, -1),
         reduction=tf.losses.Reduction.SUM_BY_NONZERO_WEIGHTS)
 
     check = tf.Assert(tf.is_finite(loss), [tf.reduce_mean(loss)])
@@ -98,10 +98,10 @@ def dice_loss(labels, logits, smooth=1., axis=None, name='dice_loss'):
 
 
 def balanced_sigmoid_cross_entropy_with_logits(
-        labels, logits, non_bg_mask, name='balanced_sigmoid_cross_entropy_with_logits'):
+        labels, logits, fg_mask, name='balanced_sigmoid_cross_entropy_with_logits'):
     with tf.name_scope(name):
-        num_positive = tf.reduce_sum(tf.to_float(non_bg_mask))
-        num_negative = tf.reduce_sum(1. - tf.to_float(non_bg_mask))
+        num_positive = tf.reduce_sum(tf.to_float(fg_mask))
+        num_negative = tf.reduce_sum(1. - tf.to_float(fg_mask))
 
         weight_positive = num_negative / (num_positive + num_negative)
         weight_negative = num_positive / (num_positive + num_negative)
@@ -141,12 +141,12 @@ def loss(labels: Detection, logits: Detection, class_loss_kwargs, name='loss'):
         class_loss = classification_loss(
             labels=labels.classification.prob,
             logits=logits.classification.unscaled,
-            non_bg_mask=fg_mask,
+            fg_mask=fg_mask,
             class_loss_kwargs=class_loss_kwargs)
 
         regr_loss = regression_loss(
             labels=labels.regression,
             logits=logits.regression,
-            non_bg_mask=fg_mask)
+            fg_mask=fg_mask)
 
         return class_loss, regr_loss
